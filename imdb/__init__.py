0665c4fb182391bb5e91bb63049ac69289f471a3
mohammadhoseinrashvand <mohammadhoseinrashvand@gmail.com>
2024-02-07 13:03:21 +0330

    Add dummy comment

--- imdb/__init__.py
+++ imdb/__init__.py
@@ -13,7 +13,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
-# dummy comment ########################
+# dummy comment ######################## 232323232323
 """
 This package can be used to retrieve information about a movie or a person
 from the IMDb database. It can fetch data through different media such as
